b986363b696cefdb953d3c4092b1c44609abd8d7
Kitkat <mr7kitkat@gmail.com>
2022-03-17 16:21:37 +0530

    Added functionality to Clear and Backspace button

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const buttons =  document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.num');
+const expression = document.querySelector('#expression');
 
-buttons.
+// Add functionality to Clear or Reser button
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', function(){
+    expression.innerText ='';
+})
+
+// Add functionality to BackSpace button
+const backspace = document.querySelector('.backspace');
+backspace.addEventListener('click', function(){
+    let currval = expression.innerText;
+    expression.innerText = currval.slice(0,currval.length - 1);
+})
+
+buttons.forEach(button => {
+    button.addEventListener('click',function(e){
+        let keyval = e.target.innerText;
+        expression.innerText += keyval;
+    })
+})
+
