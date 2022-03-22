11ad3a6c2ca8aa183da3505e2f211314f768a14c
Kitkat <mr7kitkat@gmail.com>
2022-03-22 15:58:18 +0530

    make a new brach to make calculation algorithm

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// array of character code of
-//                     (    )    ⨯     ÷   +    −     %  or mathOperators
-const mathOperators = [40, 41, 10799, 247, 43, 8722, 37];
-let expressionArray;
-
+// It takes user input on click
 const inputButtons = document.querySelectorAll('.num');
 const expressionInput = document.querySelector('#expression');
 inputButtons.forEach(button => {
@@ -12,107 +8,20 @@ inputButtons.forEach(button => {
     })
 })
 
+// Clears all the input 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', function(e){
     expressionInput.innerText = '';
 })
 
+// it clears one character from last
 const backSpaceButton = document.querySelector('#backspace');
 backSpaceButton.addEventListener('click', function(e){
     let getCurrentExpression = expressionInput.innerText;
     expressionInput.innerText = getCurrentExpression.slice(0,getCurrentExpression.length - 1);
 })
 
+// *************** THE EQUAL BUTTON **********************
+// What shits will happen when user clicks on Equal Button
 const equalButton = document.querySelector('#equal');
-equalButton.addEventListener('click',function(){
-    expressionArray = getExpressionArray(expressionInput.innerText);
-})
 
-// -----------------------------------------------------------------------------------------------
-// List of Functions    
-// -----------------------------------------------------------------------------------------------
-// Function to replace all value inside a string
-function replaceAll(str, oldValue, newValue) {
-    while(str.includes(oldValue)){
-        str = str.replace(oldValue,newValue)
-    }
-    return str;
-}
-
-function getExpressionArray(expression){
-    const returnArray = []
-    let nums = ''
-    for(let i = 0; i <= expression.length; i++){ 
-        if (i === expression.length) {
-            returnArray.push(Number(nums)); 
-            return returnArray;
-        }
-
-        if(mathOperators.includes(expression.charCodeAt(i))){
-            if(nums) {
-                returnArray.push(Number(nums));
-                nums = '';
-            }
-            returnArray.push(expression[i]);
-        }else {
-            nums += expression[i];
-        }        
-    }
-}
-
-
-function calcBrackets(expArray) {
-    let bracketStart = expArray.indexOf('('); 
-    let bracketEnd = expArray.indexOf(')'); 
-    let deleteCharacters = (bracketStart - bracketEnd) + 1;
-
-    let bracketExpression = expArray.slice(bracketStart+1, bracketEnd);
-
-    bracketExpression
-    let insert = evaluate(bracketExpression);
-
-    return expArray.splice(bracketStart, deleteCharacters, insert);
-}
-
-
-
-// function solveOperations(expArray, operator1, operator2) {
-//     let operator1Index;
-//     if (!expArray.includes(operator1)) {
-//         operator1Index = expArray.length;
-//     }else {
-//         operator1Index = expArray.indexOf(operator1);
-//     }
-    
-    
-//     let operator2Index;
-//     if (!expArray.includes(operator2)) {
-//         operator2Index = expArray.length;
-//     }else {
-//         operator2Index = expArray.indexOf(operator2);
-//     }
-    
-
-//     let min = Math.min(operator1Index, operator2Index);
-
-//     let firstArg = expArray[min - 1];
-//     let secondArg = expArray[min + 1];
-
-//     let spliceStart = expArray.indexOf(operator1);
-//     let deleteItems = 3;
-//     let result = 0;
-
-//     if (expArray.charCodeAt[min] === 10799) {
-//         result = firstArg * secondArg;
-//     }else if (expArray.charCodeAt[min] === 247) {
-//         result = firstArg / secondArg;
-//     }else if (expArray.charCodeAt[min] === 43) {
-//         result = firstArg + secondArg;
-//     }else if (expArray.charCodeAt[min] === 8722) {
-//         result = firstArg - secondArg;
-//     }
-
-//     return expArray.splice(spliceStart, deleteItems, result);
-// }
-
-// console.log(solveOperations(2,"/",2))
